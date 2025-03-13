4d17d4d070376898961311f8e1ba950cb40b645e
Naorem123 <naoremalex909@gmail.com>
2025-03-13 10:54:07 +0530

    state

---
+++ ConditionalStatement/state.java
@@ -0,0 +1,10 @@
+public class state {
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("He is good");
+        }
+        if(false){
+            System.out.println("He is not good");
+        }
+    }
+}
