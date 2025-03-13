bd8dd121de3a0ed245eb23a29b3746a90bc41da0
Naorem123 <naoremalex909@gmail.com>
2025-03-13 15:46:09 +0530

    else

---
+++ Else-if/el.java
@@ -0,0 +1,14 @@
+public class el {
+    public static void main(String args[]){
+        int a=1;
+        if(a<0){
+            System.out.println("negative");
+        }
+        else if(a>=1){
+            System.out.println("positive");
+        }
+        else{
+            System.out.println("the no is 0");
+        }
+    }
+}
