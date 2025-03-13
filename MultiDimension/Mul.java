5d8eed6ae8f1561875a1ba298b9192a83ede1ce6
Naorem123 <naoremalex909@gmail.com>
2025-03-13 13:48:11 +0530

    chu2

---
+++ MultiDimension/Mul.java
@@ -0,0 +1,10 @@
+public class Mul {
+    public static void main(String[] args) {
+              int arr[][]={{10,20,30},{40,50,70},{12,23,45}};
+   
+               System.out.println (arr[1][2]);
+               System.out.println (arr[0][1]);
+               System.out.println (arr[2][1]);
+       }
+   
+}
