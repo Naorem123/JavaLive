8266f272cac268c4174df30384d4b436046dbcb5
Naorem123 <naoremalex909@gmail.com>
2025-03-13 12:15:39 +0530

    nunu

---
+++ MajorMinorFinder/Maj.java
@@ -0,0 +1,15 @@
+
+ public class Maj {
+    
+    public static void main(String[] args) {
+        int age = 20;
+        if(age>=19){
+        System.out.println("The perso is an major");
+        
+     }
+     else{
+        System.out.println("It's minor");
+     }
+    }
+    }
+    
