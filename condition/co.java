39332a8cca849f59a8152180ebb5120d8640ad3b
Naorem123 <naoremalex909@gmail.com>
2025-03-13 15:50:22 +0530

    u

---
+++ condition/co.java
@@ -0,0 +1,16 @@
+
+public class co{
+    public static void main(String args[]){
+        int num=45;
+        if(num>0){
+            System.out.println("positive");
+        }
+        else if(num==0){
+            System.out.println("Neutral");
+        }
+        else{
+            System.out.println("Negative");
+        }
+    }
+    
+}
