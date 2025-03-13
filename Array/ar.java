6653edc7d43bcacf660638bd3ecc686690972ea8
Naorem123 <naoremalex909@gmail.com>
2025-03-13 13:26:06 +0530

    thu2

---
+++ Array/ar.java
@@ -0,0 +1,10 @@
+public class ar {
+     public static void main(String[] args) {
+           int num[]={10,20,30,40};
+
+            System.out.println(num[0]);
+            System.out.println(num[1]);
+            System.out.println(num[2]);
+            System.out.println(num[3]);
+              }
+    }
