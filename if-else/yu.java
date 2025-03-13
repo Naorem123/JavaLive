4d2eb0710ae8d1ceb78dbe89fcf4344dd938b166
Naorem123 <naoremalex909@gmail.com>
2025-03-13 11:02:44 +0530

    else

---
+++ if-else/yu.java
@@ -0,0 +1,11 @@
+public class yu{
+    public static void main(String[] args) {
+        if(true){
+            System.out.println("i'm good");
+        }
+        else{
+            System.out.println("I'm not good");
+        }
+    }
+    
+}
