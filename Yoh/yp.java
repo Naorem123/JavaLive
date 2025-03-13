47625c9b429eaa062d06cda9328a3b95d3dbe653
Naorem123 <naoremalex909@gmail.com>
2025-03-13 15:17:50 +0530

    correct

---
+++ Yoh/yp.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class yp {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter age:");
+        int age = input.nextInt();
+        System.out.println("age");
+    }
+}
+
