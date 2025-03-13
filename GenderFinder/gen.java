154ac0d6e44c92932a31cb159f5a37ee637be115
Naorem123 <naoremalex909@gmail.com>
2025-03-13 15:11:16 +0530

    luv

---
+++ GenderFinder/gen.java
@@ -0,0 +1,12 @@
+public class gen {
+     public static void main(String args[]){       
+        String a= "M";        
+         if(a=="M"){           
+      System.out.println("Male");       
+        }       
+          else
+        {           
+       System.out.println("female");      
+          } 
+        }
+     }
