5a13b728d3436f737ff7ec340780da78218df322
Naorem123 <naoremalex909@gmail.com>
2025-03-13 11:46:38 +0530

    pos

---
+++ PositiveNegative/pos.java
@@ -0,0 +1,14 @@
+public class pos {
+    public static void main(String[] args) {
+        int number = 20;
+        if(number>0){
+            System.out.println("It's positive");
+        }
+        else if(number>0){
+            System.out.println("It's negative");
+    }
+    else {
+        System.out.println("It's zero");
+}
+}
+}
