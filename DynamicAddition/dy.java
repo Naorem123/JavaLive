bbe0333840aa97357f2ccb4b6ecc105abf79923d
Naorem123 <naoremalex909@gmail.com>
2025-03-13 15:25:27 +0530

    boom

---
+++ DynamicAddition/dy.java
@@ -0,0 +1,14 @@
+ import java.util.*;
+ public class dy {
+     public static void main(String[] args) {
+         Scanner o=new Scanner(System.in);
+         System.out.println("enter any no.: " );
+         int no= o.nextInt();
+         Scanner w=new Scanner(System.in);
+         System.out.println("enter any no.: " );
+         int bn= w.nextInt();
+         int z=no+bn;
+         System.out.println("add: "+z);
+     }
+ }
+
