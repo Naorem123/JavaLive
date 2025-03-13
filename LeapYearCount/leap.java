336b0bf5d7eef284c21eef40b093c85e8059e7a3
Naorem123 <naoremalex909@gmail.com>
2025-03-13 14:47:14 +0530

    lol

---
+++ LeapYearCount/leap.java
@@ -0,0 +1,11 @@
+public class leap {
+    public static void main(String[] args) {
+        int total = 0;
+        for(int i=2020;i<2031;i++){
+        if(i%4==0){
+        total=total+1;
+        }
+    }
+    System.out.println(total);
+}
+}
