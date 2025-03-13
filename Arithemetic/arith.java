1a37c67a93c946ad8349e9e8c9d52317b12566d3
Naorem123 <naoremalex909@gmail.com>
2025-03-13 10:27:31 +0530

    ari

---
+++ Arithemetic/arith.java
@@ -0,0 +1,9 @@
+public class arith {
+    public static void main(String[] args) {
+        System.out.println(5+2);
+        System.out.println(5-2);
+        System.out.println(5*2);
+        System.out.println(5/2);
+        System.out.println(5%2);
+    }
+}
