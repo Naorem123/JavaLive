0ac2f4235765cc9e44fec00f25a97a32fa6b3c66
Naorem123 <naoremalex909@gmail.com>
2025-03-13 14:23:01 +0530

    bang

---
+++ OddNumber/odd.java
@@ -0,0 +1,10 @@
+public class odd {
+    public static void main(String[] args) {
+                for (int i=27;i<38; i++){
+                    if(i%2!=0){
+              System.out.println(i);
+            }
+          }
+        }
+    }
+    
