9e453e48d50d8803bdc43f5f6866a6963c811070
Naorem123 <naoremalex909@gmail.com>
2025-03-14 12:01:16 +0530

    low

---
+++ LowerCase/low.java
@@ -0,0 +1,7 @@
+public class low {
+public static void main(String[] args) {
+            String name = "yoh,bang";
+            System.out.println(name.toUpperCase());
+            System.out.println(name.toLowerCase());
+        }
+    }
