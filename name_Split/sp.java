bd629775072e9acc16f6f9c91c4e231bca2c3160
Naorem123 <naoremalex909@gmail.com>
2025-03-14 11:50:43 +0530

    Spli

---
+++ name_Split/sp.java
@@ -0,0 +1,9 @@
+public class sp {
+        public static void main(String args[]){
+            String name = "hell,world";
+            String n[] = name.split(",");
+            System.out.println(n[0]);
+        }
+        
+    }
+    
