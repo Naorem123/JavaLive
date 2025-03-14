d765e7538d9ba457f7e0f405ebbe920dee1bc9c0
Naorem123 <naoremalex909@gmail.com>
2025-03-14 12:06:46 +0530

    ind

---
+++ indexOf/in.java
@@ -0,0 +1,7 @@
+public class in {
+ public static void main(String[] args) {
+                    String name = "yoh,bang";
+                    System.out.println(name.indexOf("n"));
+                    
+                }
+            }
