92da65b0e240787b1b46ad29467a1614e83c6ee4
Naorem123 <naoremalex909@gmail.com>
2025-03-14 10:06:12 +0530

    lp

---
+++ Loop/lop.java
@@ -0,0 +1,9 @@
+public class lop {
+    public static void main(String[] args) {
+            int i = 92;
+            while(i<99){
+                System.out.println(i);
+                i=i+1;
+            }
+        }
+    }
