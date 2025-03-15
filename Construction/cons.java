48a70831c2ca24c0d104b808ee82576524e374fb
Naorem123 <naoremalex909@gmail.com>
2025-03-15 10:54:39 +0530

    cons

---
+++ Construction/cons.java
@@ -0,0 +1,9 @@
+public class cons {
+public cons()
+{
+    System.out.println("afdaf");
+ }
+ public static void main(String[] args) {
+    cons obj = new cons();
+ }
+}
--- Sum_Digit/sum.java
+++ Sum_Digit/sum.java
@@ -1,15 +1,13 @@
 import java.util.*;
 public class sum {
-public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a number: ");
-        int n = scanner.nextInt();
-        int sum = 0;
-         while ( n != 0) {
-            sum += n % 10; 
-            n /= 10; 
-        } 
-        System.out.println("The sum of the digits is: " + sum);
-        }
+    int sum(int x,int y, int z){
+        int a=x+y+z;
+        return a;
     }
-
+        public static void main(String[] args) {
+        sum obj=new sum();
+        int a=obj.sum(23,55,43);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("a");
+        }
+    }    
