48f0dbbc3c9d28ff5fc42603fd917439a742785b
Naorem123 <naoremalex909@gmail.com>
2025-03-15 11:38:09 +0530

    pri

--- primeNumber/pri.java
+++ primeNumber/pri.java
@@ -1,4 +1,14 @@
-import java.util.*;
 public class pri {
-          
-}
+        public static void main(String[] args) {
+            int num = 29; 
+            System.out.println(isPrime(num) ? num + " is prime." : num + " is not prime.");
+        }
+    
+        public static boolean isPrime(int num) {
+            if (num <= 1) return false;
+            for (int i = 2; i <= Math.sqrt(num); i++) {
+                if (num % i == 0) return false;
+            }
+            return true;
+        }
+    }
