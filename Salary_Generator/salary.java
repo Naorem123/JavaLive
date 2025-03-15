623235b1579ecb3025de1620d4ba2ae18e7c8b19
Naorem123 <naoremalex909@gmail.com>
2025-03-15 14:42:16 +0530

    salary

---
+++ Salary_Generator/salary.java
@@ -0,0 +1,29 @@
+import java.util.*;
+ public class salary {
+     Scanner scan = new Scanner(System.in);
+     public static void main(String[] args) {
+         Scanner scan = new Scanner(System.in);
+ 
+         System.out.println("Enter the basic salary");
+         double basic = scan.nextDouble(); 
+         System.out.println("Select an occupation: \n1:manager\n2:designer\n3:employee\n4:others");
+         int occupation = scan.nextInt();
+         double result;
+         
+         if (occupation == 1) {
+             result = basic + (0.2 * basic); 
+             System.out.println("Salary: " + result);
+         } else if (occupation == 2) {
+             result = basic + (0.1 * basic);
+             System.out.println("Salary: " + result);
+         } else if (occupation == 3) {
+             result = basic;
+             System.out.println("Salary: " + result);
+         } else if (occupation == 4) {
+             result = basic - (0.05 * basic);
+             System.out.println("Salary: " + result);
+         } else {
+             System.out.println("Invalid input");       
+         }   
+      }
+    }
